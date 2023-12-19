9eea3f8d87b30a6e7e3fa528899a92e57d85e127
Shatalov.AG <mrstep93@ya.ru>
2023-12-20 01:10:43 +0300

    новый проект не в ту ветвь запилил((

---
+++ m7_3_abstractClasses/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace m7_3_abstractClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
